b3365983d97e41f451b888b89a9a9f7eafab27b2
arasHi87 <arasi27676271@gmail.com>
2020-04-20 15:45:01 +0800

    fix var name and add epub maker process

--- NovelBK/pipelines.py
+++ NovelBK/pipelines.py
@@ -8,6 +8,7 @@
 import os
 import redis
 from scrapy import Request
+from NovelBK.utils import *
 from NovelBK import settings
 from NovelBK.items import Wenku8IndexItem
 from scrapy.exporters import JsonItemExporter
@@ -15,14 +16,14 @@ from scrapy.exporters import JsonItemExporter
 class Wenku8IndexPipeline(object):
     def process_item(self, item, spider):
         if isinstance(item, Wenku8IndexItem):
-            book_name, content = list(item['index'].items())[0]
+            b_name, content = list(item['index'].items())[0]
             loc = os.path.dirname(__file__)
-            json_file_path = os.path.join(loc, 'data/wenku8', book_name)
+            json_file_path = os.path.join(loc, 'data/wenku8', b_name)
 
             if not os.path.isdir(json_file_path):
                 os.makedirs(json_file_path)
 
-            json_file_nod = os.path.join(json_file_path, book_name +  '.json')
+            json_file_nod = os.path.join(json_file_path, b_name +  '.json')
 
             if not os.path.exists(json_file_nod):
                 os.mknod(json_file_nod)
@@ -33,6 +34,24 @@ class Wenku8IndexPipeline(object):
             self.exporter.export_item(item['index'])
             self.exporter.finish_exporting()
             self.file.close()
+
+            # chake file exists and make epub file
+            for volume in content:
+                v_name = volume[0]
+                file_status = True
+                volume = volume[1:]
+
+                if file_status:
+                    for c_name in volume:
+                        if not os.path.exists(os.path.join(json_file_path, 'txt', v_name, c_name + '.txt')):
+                            file_status = False
+                            break
+                else:
+                    break
+                
+            if file_status:
+                txt2epub(item, os.path.join(loc, 'data/wenku8'))
+
         return item
     
     def open_spider(self, spider):
