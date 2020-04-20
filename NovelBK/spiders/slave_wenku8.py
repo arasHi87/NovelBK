4eaf4090d30f2642171c4c7128d9e6c18994e08d
arasHi87 <arasi27676271@gmail.com>
2020-04-20 15:04:09 +0800

    fix var name and use os.path.dirname to locate file

--- NovelBK/spiders/slave_wenku8.py
+++ NovelBK/spiders/slave_wenku8.py
@@ -14,65 +14,72 @@ class Wenku8SlaveSpider(RedisSpider):
     allow_domains = ['wenku8.net']
 
     def parse(self, response):
-        aid =  response.url.split('/')[-2]
-        book_name = response.xpath('//*[@id="title"]/text()').get()
+        '''
+        b_name: book name
+        c_name: chapter name
+        v_name: volume name
+        '''
 
+        aid = response.url.split('/')[-2]
+        b_name = response.xpath('//*[@id="title"]/text()').get()
+        
         # build index information
-        i_chapter = ''
         temp = []
         item = Wenku8IndexItem()
         item['index'] = {}
-        item['index'][book_name] = []
+        item['index'][b_name] = []
+
         for ele in response.xpath('//table/tr/td'):
             n_type = ele.xpath('@class').get()
             if n_type == 'vcss':
                 if temp:
-                    item['index'][book_name].append(temp)
+                    item['index'][b_name].append(temp)
                 temp = [normalize('NFKD', ele.xpath('text()').get())]
             else:
                 if ele.xpath('a/text()').get():
-                    n_name = normalize('NFKD', ele.xpath('a/text()').get())
-                    if n_name != '插图':
-                        temp.append(n_name)
-        item['index'][book_name].append(temp)
+                    c_name = normalize('NFKD', ele.xpath('a/text()').get())
+                    if c_name != '插图':
+                        temp.append(c_name)
+
+        item['index'][b_name].append(temp)
         yield item
 
         # get content
         for x in response.xpath("//table/tr/td[@class='ccss']/a"):
             vid = x.xpath('@href').get().replace('.htm', '')
-            vname = x.xpath('text()').get()
+            c_name = x.xpath('text()').get()
             url = response.url.replace('index', vid)
-
-            if not self.server.hget(self.settings.get('REDIS_DATA_DICT'), url):
+            if not self.server.hget(self.settings.get('REDIS_DATA_DICT'), url) or self.settings.get('DEBUG_MOD'):
                 yield Request(
                     url = url,
                     meta = {
                         'aid': aid,
                         'vid': vid,
-                        'vname': normalize('NFKD', vname),
-                        'book_name': book_name
+                        'c_name': normalize('NFKD', c_name),
+                        'b_name': b_name
                     },
                     callback = self.parse_chapter
                 )
     
     def parse_chapter(self, response):
         cc = OpenCC('s2t')
+        loc = os.path.dirname(__file__)
         content = BeautifulSoup(response.xpath('//*[@id="content"]').get().strip(), "lxml").text
-        chapter = "".join(normalize('NFKD',
-            response.xpath('//*[@id="title"]/text()').get()).rsplit(response.meta['vname'], 1)).strip()
-        path = os.path.join('data', response.meta['book_name'], chapter)
+        title = normalize('NFKD', response.xpath('//*[@id="title"]/text()').get())
+        v_name = "".join(title.rsplit(response.meta['c_name'], 1)).strip()
+        path = os.path.join(loc, '../data/wenku8', response.meta['b_name'], v_name)
 
         if not os.path.isdir(path):
             os.makedirs(path)
 
         if '因版权问题，文库不再提供该小说的阅读！' in content:
             url = self.settings.get('WENKU8_DOWNLOAD_URL').format(
-                    response.meta['aid'],
-                    response.meta['vid'])
-            urlretrieve(url, filename = os.path.join(path, response.meta['vname'] + '.txt'))
+                response.meta['aid'],
+                response.meta['vid'])
+            urlretrieve(url, filename = os.path.join(path, response.meta['v_name'] + '.txt'))
         else:
-            if response.meta['vname'] != '插图':
-                with open(os.path.join(path, response.meta['vname'] + '.txt'), 'w+') as fp:
+            if response.meta['c_name'] != '插图':
+                with open(os.path.join(path, response.meta['c_name'] + '.txt'), 'w+') as fp:
                     fp.write(cc.convert(content))
-
+        
         self.server.hset(self.settings.get('REDIS_DATA_DICT'), response.url, 0)
