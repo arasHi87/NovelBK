a8bb6f532506a80d75f241af1fb5031176d155b3
arasHi87 <arasi27676271@gmail.com>
2020-04-21 13:38:07 +0800

    add json record

--- NovelBK/pipelines.py
+++ NovelBK/pipelines.py
@@ -6,6 +6,7 @@
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
 import os
+import json
 import redis
 from scrapy import Request
 from NovelBK.utils import *
@@ -16,41 +17,15 @@ from scrapy.exporters import JsonItemExporter
 class Wenku8IndexPipeline(object):
     def process_item(self, item, spider):
         if isinstance(item, Wenku8IndexItem):
-            # b_name, content = list(item['index'].items())[0]
-            # loc = os.path.dirname(__file__)
-            # json_file_path = os.path.join(loc, 'data/wenku8', b_name)
+            b_name, content = list(item['index'].items())[0]
+    
+            loc = os.path.dirname(__file__)
+            path = os.path.join(loc, 'data/data.json')
+            data = json.load(open(path))
+            data['wenku8'][b_name] = content
 
-            # if not os.path.isdir(json_file_path):
-            #     os.makedirs(json_file_path)
-
-            # json_file_nod = os.path.join(json_file_path, b_name +  '.json')
-
-            # if not os.path.exists(json_file_nod):
-            #     os.mknod(json_file_nod)
-
-            # self.file = open(json_file_nod, 'wb+')
-            # self.exporter = JsonItemExporter(self.file, encoding='utf-8', ensure_ascii=False)
-            # self.exporter.start_exporting()
-            # self.exporter.export_item(item['index'])
-            # self.exporter.finish_exporting()
-            # self.file.close()
-
-            # # chake file exists and make epub file
-            # for volume in content:
-            #     v_name = volume[0]
-            #     file_status = True
-            #     volume = volume[1:]
-
-            #     if file_status:
-            #         for c_name in volume:
-            #             if not os.path.exists(os.path.join(json_file_path, 'txt', v_name, c_name + '.txt')):
-            #                 file_status = False
-            #                 break
-            #     else:
-            #         break
-                
-            # if file_status:
-            #     txt2epub(item, os.path.join(loc, 'data/wenku8'))
+            with open(path, 'w') as fp:
+                fp.write(json.dumps(data, ensure_ascii = False, indent = 4))
 
             txt2epub(item, os.path.join(loc, 'data/wenku8'))
 
