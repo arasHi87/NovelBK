1c4b23f6420caea533712abc6a3c6951d3e1c3ad
arasHi87 <arasi27676271@gmail.com>
2020-04-23 09:23:04 +0800

    change master add url way

--- NovelBK/spiders/master.py
+++ NovelBK/spiders/master.py
@@ -7,18 +7,20 @@ r = redis.Redis(host = settings.REDIS_HOST, port = settings.REDIS_PORT)
 
 class MasterWenku8Spider(scrapy.Spider):
     name = 'master_wenku8'
-    start_urls = [
-        'https://www.wenku8.net/book/1.htm'
-    ]
+    start_urls = []
+
+    for i in range(1, settings.WENKU8_MAX_AID):
+        start_urls.append(
+            'https://www.wenku8.net/book/{}.htm'.format(i)
+        )
 
     def parse(self, response):
         is_exist = response.xpath('//div[5]/div/div/div[1]/table[1]/tr[1]/td/table/tr/td[1]/span/b/text()').get()
         aid = response.url.split('/')[-1].split('.')[0]
 
-        if is_exist or int(aid) < settings.WENKU8_MAX_AID:
+        if is_exist:
             url = response.url.replace(aid + '.htm', str(int(aid) + 1) + '.htm')
             yield scrapy.Request(url = url, callback = self.parse)
-            if is_exist:
-                r.lpush('NovelBK:start_urls',
-                    response.xpath('//div[5]/div/div/div[1]/div[4]/div/span[1]/fieldset/div/a/@href').get())
+            r.lpush('NovelBK:start_urls',
+                response.xpath('//div[5]/div/div/div[1]/div[4]/div/span[1]/fieldset/div/a/@href').get())
                 
