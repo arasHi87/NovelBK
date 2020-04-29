0552233eb896a61b78aa38c7982369adae2f5567
arasHi87 <arasi27676271@gmail.com>
2020-04-29 16:54:59 +0800

    add temp folder and OpenCC

--- NovelBK/spiders/slave_wenku8.py
+++ NovelBK/spiders/slave_wenku8.py
@@ -5,6 +5,7 @@ from scrapy import Request
 from bs4 import BeautifulSoup
 from unicodedata import normalize
 from urllib.request import urlretrieve
+from NovelBK.utils import ffname
 from NovelBK.items import Wenku8IndexItem
 from scrapy_redis.spiders import RedisSpider
 
@@ -13,74 +14,174 @@ class Wenku8SlaveSpider(RedisSpider):
     redis_key = 'NovelBK:start_urls'
     allow_domains = ['wenku8.net']
 
+    # def parse(self, response):
+    #     '''
+    #     b_name: book name
+    #     c_name: chapter name
+    #     v_name: volume name
+    #     '''
+
+    #     cc = OpenCC('s2t')
+    #     aid = response.url.split('/')[-2]
+    #     b_name = cc.convert(response.xpath('//*[@id="title"]/text()').get())
+        
+    #     # build index information
+    #     temp = []
+    #     item = Wenku8IndexItem()
+    #     item['index'] = {}
+    #     item['index'][b_name] = {}
+    #     item['index'][b_name]['index'] = []
+    #     item['index'][b_name]['author'] = cc.convert(response.xpath('//*[@id="info"]/text()').get().replace('作者：', ''))
+
+    #     for ele in response.xpath('//table/tr/td'):
+    #         n_type = ele.xpath('@class').get()
+    #         if n_type == 'vcss':
+    #             if temp:
+    #                 item['index'][b_name]['index'].append(temp)
+    #             temp = [cc.convert(normalize('NFKD', ele.xpath('text()').get()))]
+    #         else:
+    #             if ele.xpath('a/text()').get():
+    #                 c_name = cc.convert(normalize('NFKD', ele.xpath('a/text()').get()))
+    #                 temp.append(c_name)
+
+    #     item['index'][b_name]['index'].append(temp)
+    #     # yield item
+
+    #     url_check_list = [response.url.replace('index.htm', x.get()) for x in response.xpath("//table/tr/td[@class='ccss']/a/@href")]
+
+    #     # get content
+    #     for x in response.xpath("//table/tr/td[@class='ccss']/a"):
+    #         vid = x.xpath('@href').get().replace('.htm', '')
+    #         c_name = cc.convert(x.xpath('text()').get())
+    #         url = response.url.replace('index', vid)
+    #         if not self.server.hget(self.settings.get('REDIS_DATA_DICT'), url) or self.settings.get('DEBUG_MOD'):
+    #             yield Request(
+    #                 url = url,
+    #                 meta = {
+    #                     'aid': aid,
+    #                     'vid': vid,
+    #                     'c_name': normalize('NFKD', c_name),
+    #                     'b_name': b_name,
+    #                     'item': item,
+    #                     'url_check_list': url_check_list
+    #                 },
+    #                 callback = self.parse_chapter
+    #     #         )
+    
+    # def parse_chapter(self, response):
+    #     cc = OpenCC('s2t')
+    #     loc = os.path.dirname(__file__)
+    #     content = cc.convert(BeautifulSoup(response.xpath('//*[@id="content"]').get().strip(), "lxml").text)
+    #     title = cc.convert(normalize('NFKD', response.xpath('//*[@id="title"]/text()').get()))
+    #     v_name = cc.convert("".join(title.rsplit(response.meta['c_name'], 1)).strip())
+    #     path = os.path.join(loc, self.settings.get('TEMP_STORE_PATH') 'wenku8', response.meta['b_name'], 'txt', v_name)
+
+    #     if not os.path.isdir(path):
+    #         os.makedirs(path)
+
+    #     if '因版权问题，文库不再提供该小说的阅读！' in content:
+    #         url = self.settings.get('WENKU8_DOWNLOAD_URL').format(
+    #             response.meta['aid'],
+    #             response.meta['vid'])
+    #         urlretrieve(url, filename = os.path.join(path, response.meta['c_name'] + '.txt'))
+    #     else:
+    #         if response.meta['c_name'] != '插图':
+    #             with open(os.path.join(path, response.meta['c_name'] + '.txt'), 'w+') as fp:
+    #                 fp.write(content)
+    #         else:
+    #             with open(os.path.join(path, response.meta['c_name'] + '.txt'), 'w+') as fp:
+    #                 for img in response.xpath('//*[@class="divimage"]').getall():
+    #                     fp.write(img)
+        
+    #     self.server.hset(self.settings.get('REDIS_DATA_DICT'), response.url, 0)
+    #     file_status = True
+
+    #     for url in response.meta['url_check_list']:
+    #         if not self.server.hget(self.settings.get('REDIS_DATA_DICT'), url):
+    #             file_status = False
+    #             break
+        
+    #     if file_status:
+    #         yield response.meta['item']
+
     def parse(self, response):
         '''
+        a_id: book id
+        v_id: volume id
+
         b_name: book name
         c_name: chapter name
         v_name: volume name
         '''
 
-        aid = response.url.split('/')[-2]
         b_name = response.xpath('//*[@id="title"]/text()').get()
-        
+        a_id = response.url.split('/')[-2]
+        cc = OpenCC('s2t')
+
         # build index information
         temp = []
         item = Wenku8IndexItem()
         item['index'] = {}
         item['index'][b_name] = {}
         item['index'][b_name]['index'] = []
-        item['index'][b_name]['author'] = response.xpath('//*[@id="info"]/text()').get().replace('作者：', '')
+        item['index'][b_name]['author'] = cc.convert(
+            response.xpath('//*[@id="info"]/text()').get().replace('作者：', '')
+        )
 
         for ele in response.xpath('//table/tr/td'):
             n_type = ele.xpath('@class').get()
+
             if n_type == 'vcss':
                 if temp:
                     item['index'][b_name]['index'].append(temp)
-                temp = [normalize('NFKD', ele.xpath('text()').get())]
+                v_name = ffname(
+                    cc.convert(normalize('NFKD', ele.xpath('text()').get())))
+                # v_name = cc.convert(normalize('NFKD', ele.xpath('text()').get())).replace('/', '')
+                temp = [v_name]
             else:
                 if ele.xpath('a/text()').get():
-                    c_name = normalize('NFKD', ele.xpath('a/text()').get())
+                    c_name = ffname(cc.convert(normalize('NFKD', ele.xpath('a/text()').get())))
+                    # c_name = cc.convert(normalize('NFKD', ele.xpath('a/text()').get())).replace('/', '')
                     temp.append(c_name)
 
         item['index'][b_name]['index'].append(temp)
-        # yield item
-
+        
         url_check_list = [response.url.replace('index.htm', x.get()) for x in response.xpath("//table/tr/td[@class='ccss']/a/@href")]
-
+        
         # get content
         for x in response.xpath("//table/tr/td[@class='ccss']/a"):
-            vid = x.xpath('@href').get().replace('.htm', '')
+            v_id = x.xpath('@href').get().replace('.htm', '')
             c_name = x.xpath('text()').get()
-            url = response.url.replace('index', vid)
+            url = response.url.replace('index', v_id)
             if not self.server.hget(self.settings.get('REDIS_DATA_DICT'), url) or self.settings.get('DEBUG_MOD'):
                 yield Request(
                     url = url,
                     meta = {
-                        'aid': aid,
-                        'vid': vid,
-                        'c_name': normalize('NFKD', c_name),
+                        'a_id': a_id,
+                        'v_id': v_id,
+                        'c_name': cc.convert(ffname(normalize('NFKD', c_name))),
                         'b_name': b_name,
                         'item': item,
                         'url_check_list': url_check_list
                     },
                     callback = self.parse_chapter
                 )
-    
+
     def parse_chapter(self, response):
         cc = OpenCC('s2t')
         loc = os.path.dirname(__file__)
         content = BeautifulSoup(response.xpath('//*[@id="content"]').get().strip(), "lxml").text
-        title = normalize('NFKD', response.xpath('//*[@id="title"]/text()').get())
-        v_name = "".join(title.rsplit(response.meta['c_name'], 1)).strip()
-        path = os.path.join(loc, '../data/wenku8', response.meta['b_name'], 'txt', v_name)
+        title = cc.convert(ffname(normalize('NFKD', response.xpath('//*[@id="title"]/text()').get())))
+        v_name = ffname("".join(title.rsplit(response.meta['c_name'], 1)).strip())
+        path = os.path.join(loc, '../', self.settings.get('TEMP_STORE_PATH'), response.meta['b_name'], 'txt', v_name)
 
         if not os.path.isdir(path):
             os.makedirs(path)
 
         if '因版权问题，文库不再提供该小说的阅读！' in content:
             url = self.settings.get('WENKU8_DOWNLOAD_URL').format(
-                response.meta['aid'],
-                response.meta['vid'])
+                response.meta['a_id'],
+                response.meta['v_id'])
             urlretrieve(url, filename = os.path.join(path, response.meta['c_name'] + '.txt'))
         else:
             if response.meta['c_name'] != '插图':
