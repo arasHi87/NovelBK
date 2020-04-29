b313f197afd829206872dbd3d1508910ba51fc17
arasHi87 <arasi27676271@gmail.com>
2020-04-30 00:25:19 +0800

    change save file place from localhost to database

--- NovelBK/pipelines.py
+++ NovelBK/pipelines.py
@@ -9,6 +9,8 @@ import os
 import json
 import redis
 import shutil
+import gridfs
+import logging
 import pymongo
 from scrapy import Request
 from NovelBK.utils import *
@@ -20,6 +22,7 @@ class Wenku8IndexPipeline(object):
     def process_item(self, item, spider):
         if isinstance(item, Wenku8IndexItem):
             loc = os.path.dirname(__file__)
+            logger = logging.getLogger('Wenku8SlaveSpider')
             b_name, content = list(item['index'].items())[0]
             client = pymongo.MongoClient("mongodb://{}:{}@{}:{}".format(
                 settings.DB_USER,
@@ -35,12 +38,30 @@ class Wenku8IndexPipeline(object):
                 'index': json.dumps(content['index'], ensure_ascii = False)
             }
             
+            # insert/update book index to database
             if not col.find({'b_name': b_name}).count():
                 col.insert_one(data)
             else:
                 col.update_one({'b_name': b_name}, {'$set': {'index': data['index']}})
 
-            txt2epub(item, os.path.join(loc, settings.TEMP_STORE_PATH))
+            # save epub to database
+            try:
+                txt2epub(item, os.path.join(loc, settings.TEMP_STORE_PATH))
+                fs = gridfs.GridFS(db, 'wenku8_file')
+                
+                for volume in content['index']:
+                    epub_name = b_name + volume[0] + '.epub'
+                    query = {'filename': epub_name}
+
+                    if fs.exists(query):
+                        fs.delete(fs.find_one(query)._id)
+                    
+                    with open(os.path.join(loc, settings.TEMP_STORE_PATH, b_name, 'epub', epub_name), 'rb') as fp:
+                        _id = fs.put(fp.read(), filename = epub_name)
+                        logger.info('save {} successful, ObjID is {}'.format(epub_name, _id))
+                        fp.close()                        
+            except:
+                logger.warn('Error happend when making epub or insert data, please try later')
 
         return item
     
