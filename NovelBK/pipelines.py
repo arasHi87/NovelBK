ec31fddaccb624f42f5f2f40fffa03119956ac20
arasHi87 <arasi27676271@gmail.com>
2020-04-29 16:54:28 +0800

    add temp folder path

--- NovelBK/pipelines.py
+++ NovelBK/pipelines.py
@@ -8,6 +8,7 @@
 import os
 import json
 import redis
+import shutil
 import pymongo
 from scrapy import Request
 from NovelBK.utils import *
@@ -18,6 +19,7 @@ from scrapy.exporters import JsonItemExporter
 class Wenku8IndexPipeline(object):
     def process_item(self, item, spider):
         if isinstance(item, Wenku8IndexItem):
+            loc = os.path.dirname(__file__)
             b_name, content = list(item['index'].items())[0]
             client = pymongo.MongoClient("mongodb://{}:{}@{}:{}".format(
                 settings.DB_USER,
@@ -38,11 +40,25 @@ class Wenku8IndexPipeline(object):
             else:
                 col.update_one({'b_name': b_name}, {'$set': {'index': data['index']}})
 
-            txt2epub(item, os.path.join(loc, 'data/wenku8'))
+            txt2epub(item, os.path.join(loc, settings.TEMP_STORE_PATH))
 
         return item
     
     def open_spider(self, spider):
+        loc = os.path.dirname(__file__)
+        path = os.path.join(loc, settings.TEMP_STORE_PATH)
+        if not os.path.isdir(path):
+            os.mkdir(path)
+        else:
+            shutil.rmtree(path)
+            os.mkdir(path)
+
         r = redis.Redis(host = settings.REDIS_HOST, port = settings.REDIS_PORT)
         if r.hlen(settings.REDIS_DATA_DICT) == 0:
             r.hset(settings.REDIS_DATA_DICT, 'field', 0)
+    
+    def close_spider(self, spider):
+        shutil.rmtree(os.path.join(
+            os.path.dirname(__file__),
+            settings.TEMP_STORE_PATH
+        ))
